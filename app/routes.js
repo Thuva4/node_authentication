2371556fb1174fe2c14cd284ab08178c4531c32b
thuva4 <tharma.thuva@gmail.com>
2017-04-08 18:52:03 +0530

    create login functionality

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ module.exports = function(app,passport){
   });
 
   app.get('/login',function(req,res){
-    res.render('login.ejs');
+    res.render('login.ejs',{ message: req.flash('loginMessage') });
   });
 
   app.get('/signup',function(req,res){
@@ -17,7 +17,11 @@ module.exports = function(app,passport){
     failureRedirect:'/signup',
     failureFlash:true
   }));
-
+  app.post('/login',passport.authenticate('local-login',{
+    successRedirect: '/profile',
+    failureRedirect:'/login',
+    failureFlash:true
+  }));
   app.get('/profile',isLoggedIn,function(req,res){
     res.render('profile.ejs',{
       user:req.user
